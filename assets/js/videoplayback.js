9c84388260dc09679818fc7c87ced81286cff1e9
kvsphantom <kvsphantom@gmail.com>
2021-04-06 16:18:22 +0200

    fixed videoplayback of all the videos

--- assets/js/videoplayback.js
+++ assets/js/videoplayback.js
@@ -1,14 +1,16 @@
 $('.videoplay').on('click', function () {
-    var videoURL = $("#qm").prop('src');
+    var vid = $(this).attr('videoid');
+    var videoURL = $(vid).prop('src');
     videoURL = videoURL.replace("&autoplay=1", "");
-    $('#qm').prop('src','');
-    $('#qm').prop('src',videoURL);
-    $("#qm")[0].src += "&autoplay=1";
+    $(vid).prop('src','');
+    $(vid).prop('src',videoURL);
+    $(vid)[0].src += "&autoplay=1";
 });
 
 $('.videostop').on('click', function () {
-    var videoURL = $('#qm').prop('src');
+    var vid = $(this).attr('videoid');
+    var videoURL = $(vid).prop('src');
     videoURL = videoURL.replace("&autoplay=1", "");
-    $('#qm').prop('src','');
-    $('#qm').prop('src',videoURL);
+    $(vid).prop('src','');
+    $(vid).prop('src',videoURL);
 });
