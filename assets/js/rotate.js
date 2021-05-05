7dbb0fb6db583c9996f0db50a60f1e1867c2a3c3
kvsphantom <kvsphantom@gmail.com>
2021-05-05 19:55:56 +0200

    trying to fix the lazy loading of grid

--- assets/js/rotate.js
+++ assets/js/rotate.js
@@ -3,6 +3,12 @@ jQuery_1_8_3(function () {
             rows:3,
             animSpeed: 300,
             animType: 'rotateBottom',
+            w1920: {
+                rows:3,
+            },
+            w1024: {
+                rows:3,
+            },
             w320: {
                 rows: 3,
                 columns: 4
