59548b441835b7fabd085192d4fc0094f6cf2332
kvsphantom <kvsphantom@gmail.com>
2021-05-05 19:37:32 +0200

    grid rotator images workinggit add .

---
+++ assets/js/rotate.js
@@ -0,0 +1,18 @@
+jQuery_1_8_3(function () {
+        jQuery_1_8_3('#ri-grid').gridrotator({
+            rows:3,
+            animSpeed: 300,
+            animType: 'rotateBottom',
+            w320: {
+                rows: 3,
+                columns: 4
+            },
+            w240: {
+                rows: 3,
+                columns: 3
+            },
+            slideshow: true,
+            onhover: true
+        });
+
+    });
